aef1484e8cdcc0d3799ced45bc6001d614696f4f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 10:50:02 -0300

    Adicionando arquivos html e javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+/*Lista de variáveis*/ 
+
+let nome = "Larissa Estella";
+let idade = 18;
+let loira = false;
+let valorProduto = 10.50;
+let listaTimes = ['Corinthians', 'Palmeiras', 'São Paulo'];
+
+console.log(nome)
+console.log(idade)
+console.log(loira)
+console.log(valorProduto)
+console.log(listaTimes)
+
+
+/*Operações Matemáticas*/
+
+let num1 = 10;
+let num2 = 2;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let muliplicacao = num1 * num2;
+let divisao = num1/num2;
+
+console.log("O resultado da soma é: " + soma);
+console.log("O resultado da subtracao é: " + subtracao);
+console.log("O resultado da muliplicacao é: " + muliplicacao);
+console.log("O resultado da divisao é: " + divisao);
